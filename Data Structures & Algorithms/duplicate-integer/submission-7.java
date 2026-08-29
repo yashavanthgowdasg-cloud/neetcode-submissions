caa0717e53f8259e5c876727bd694d5115a24c25
yashavanthgowdasg-cloud <yashavanthgowdasg@gmail.com>
2026-08-29 08:05:03 +0530

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+       Hashtable<Integer, Integer> nd = new Hashtable<>();
+        
+        for (int i=0;i<nums.length;i++){
+            
+            if(nd.containsKey(nums[i]) == false){
+                nd.put(nums[i],1);
+            }
+            else return true;
+        }
+        return false;    
+}
+
+}
